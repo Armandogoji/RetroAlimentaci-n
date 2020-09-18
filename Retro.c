5a23da735a44b62ae129832ecfcdc6d414b3aefe
Angel Armando <angel.gomez1394@alumnos.udg.mx>
2020-09-18 15:09:11 -0500

    Capturamos los 5 numeros

--- Retro.c
+++ Retro.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+void Capturar_Enteros(){
+    int enteros[5];
+    float promedio;
+    int suma = 0;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        scanf("%i", &enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%i\n", enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        suma = suma + enteros[i];
+    }
+    printf("Suma: %i\n", suma);
+    printf("Primedio: %f\n", suma/5.0);
+    
+}
+
+
 int main(){
     char op;
 
@@ -10,6 +33,22 @@ int main(){
       printf("3) Agregar Personaje\n");
       printf("0) Salir\n");
       scanf("%c", &op);
+
+    switch (op)
+    {
+    case '1':
+        Capturar_Enteros();
+        break;
+    case '2':
+
+        break;
+    case '3':
+
+        break;
+    default:
+        break;
+    }
+
       fflush(stdin);
 
     } while (op != '0');
